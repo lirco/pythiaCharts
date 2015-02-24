a0622b059b96c742f169e506b348e9f6eec53be5
lirco <lirco77@gmail.com>
2015-02-24 10:00:05 +0200

    upgraded angular version to 1.3.13

--- chrome_extension/application.js
+++ chrome_extension/application.js
@@ -1,63 +1,72 @@
 angular.module('drops', ['ui.router', 'ngResource'])
-  .config(function ($stateProvider) {
+  .config([
 
-  $stateProvider
+    '$resourceProvider',
+    '$stateProvider',
 
-    .state('home', {
-      templateUrl: 'views/home/home.chrome.view.html',
-      controller: 'homeController',
-      controllerAs: 'homeCtrl',
-      resolve: {
-        activeTabUrl: function(AppState) {
-          return AppState.getActiveTabUrl()
-        },
-        activeTabDomain: function(AppState) {
-          return AppState.getActiveTabDomain()
-        },
-        domainNotes: function(Notes, AppState){
-          return  Notes.getNotes({domain: AppState.getActiveTabDomain()})
+    function ($resourceProvider, $stateProvider) {
+      
+    // Don't strip trailing slashes from calculated URLs
+    $resourceProvider.defaults.stripTrailingSlashes = false;
+
+
+    $stateProvider
+
+      .state('home', {
+        templateUrl: 'views/home/home.chrome.view.html',
+        controller: 'homeController',
+        controllerAs: 'homeCtrl',
+        resolve: {
+          activeTabUrl: function(AppState) {
+            return AppState.getActiveTabUrl()
+          },
+          activeTabDomain: function(AppState) {
+            return AppState.getActiveTabDomain()
+          },
+          domainNotes: function(Notes, AppState){
+            return  Notes.getNotes({domain: AppState.getActiveTabDomain()})
+          }
         }
-      }
-    })
-    .state('newNote', {
-      templateUrl: 'views/notes/newNote.chrome.view.html',
-      controller: 'notesController',
-      controllerAs: 'notesCtrl',
-      resolve: {
-        activeTabUrl: function(AppState) {
-          return AppState.getActiveTabUrl()
-        },
-        activeTabDomain: function(AppState) {
-          return AppState.getActiveTabDomain()
+      })
+      .state('newNote', {
+        templateUrl: 'views/notes/newNote.chrome.view.html',
+        controller: 'notesController',
+        controllerAs: 'notesCtrl',
+        resolve: {
+          activeTabUrl: function(AppState) {
+            return AppState.getActiveTabUrl()
+          },
+          activeTabDomain: function(AppState) {
+            return AppState.getActiveTabDomain()
+          }
         }
-      }
-    })
-    .state('editNote', {
-      templateUrl: 'views/notes/editNote.chrome.view.html',
-      controller: 'notesController',
-      controllerAs: 'notesCtrl'
-    })
+      })
+      .state('editNote', {
+        templateUrl: 'views/notes/editNote.chrome.view.html',
+        controller: 'notesController',
+        controllerAs: 'notesCtrl'
+      })
 
-    //User routes
-    .state('signIn', {
-      templateUrl: 'views/users/authentication/signIn.chrome.view.html',
-      controller: 'authenticationController',
-      controllerAs: 'authCtrl'
-    })
-    .state('signUp', {
-      templateUrl: 'views/users/authentication/signUp.chrome.view.html',
-      controller: 'authenticationController',
-      controllerAs: 'authCtrl'
-    })
-    .state('forgot', {
-      templateUrl: 'views/users/password/forgotPassword.chrome.view.html',
-      controller: 'passwordController',
-      controllerAs: 'passwordCtrl'
-    })
-    .state('passwordResetSuccess', {
-      templateUrl: 'views/users/password/passwordResetSuccess.chrome.view.html',
-      controller: 'passwordController',
-      controllerAs: 'passwordCtrl'
-    })
+      //User routes
+      .state('signIn', {
+        templateUrl: 'views/users/authentication/signIn.chrome.view.html',
+        controller: 'authenticationController',
+        controllerAs: 'authCtrl'
+      })
+      .state('signUp', {
+        templateUrl: 'views/users/authentication/signUp.chrome.view.html',
+        controller: 'authenticationController',
+        controllerAs: 'authCtrl'
+      })
+      .state('forgot', {
+        templateUrl: 'views/users/password/forgotPassword.chrome.view.html',
+        controller: 'passwordController',
+        controllerAs: 'passwordCtrl'
+      })
+      .state('passwordResetSuccess', {
+        templateUrl: 'views/users/password/passwordResetSuccess.chrome.view.html',
+        controller: 'passwordController',
+        controllerAs: 'passwordCtrl'
+      })
 
-});
+}]);
