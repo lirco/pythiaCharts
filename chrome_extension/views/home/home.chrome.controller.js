2fe7682f4ee37e7062a277967caacee5028410fb
lirco <lirco77@gmail.com>
2015-03-06 19:27:06 +0200

    delete note and homeCtrl changes in progress

--- chrome_extension/views/home/home.chrome.controller.js
+++ chrome_extension/views/home/home.chrome.controller.js
@@ -8,36 +8,74 @@
     self.authentication.user = Authentication.getUser();
     //TODO: move this to config page
     self.viewState = 'Page';
+    self.notesToShow = null;
 
-    $scope.$on('viewEvent:changeViewState', function(type, data){
-      self.viewState = data;
-      if (self.viewState == 'Page' ){
+    self.switchViewState = function(viewState){
+      if (viewState == 'Page') {
+        self.viewState = 'Page';
         self.notesToShow = self.urlNotes;
-      } else if (self.viewState == 'Site') {
+      } else if (viewState == 'Site'){
+        self.viewState = 'Site';
         self.notesToShow = self.domainNotes;
       }
-    });
+    };
 
-    self.urlNotes = [];
     self.domainNotes = [];
+    self.urlNotes = [];
 
-    Notes.getNotes({domain: activeTabDomain}, function(notes) {
-      self.domainNotes = notes;
-      for (var i=0; i < notes.length; i++) {
-        var note = notes[i];
+    self.generateUrlNotes = function (domainNotes, callback) {
+      self.urlNotes = [];
+      for (var i=0; i < domainNotes.length; i++) {
+        var note = domainNotes[i];
         if (note.url == activeTabUrl) {
           self.urlNotes.push(note);
         }
       }
+      console.log('***************************************');
+      console.log('urlNotes' + self.urlNotes);
+      console.log('***************************************');
+      if (callback) {
+        callback();
+      }
+    };
+
+    Notes.getNotes({domain: activeTabDomain}, function(notes) {
+      self.domainNotes = notes;
+      self.generateUrlNotes(notes, self.defineNotesToShow())
     });
+
+
+    self.defineNotesToShow = function() {
+      if (self.viewState == 'Page') {
+        self.notesToShow = self.urlNotes;
+      } else if (self.viewState == 'Site') {
+        self.notesToShow = self.domainNotes;
+      }
+      console.log('***************************************');
+      console.log('To show ' + self.notesToShow);
+      console.log('***************************************');
+    };
+
+    //$scope.$on('viewEvent:changeViewState', function(type, data){
+    //  self.viewState = data;
+    //  self.defineNotesToShow()
+    //});
 
     self.editNote = function(note) {
       AppState.setActiveNote(note);
       $state.go('editNote')
     };
 
-    //TODO: move this to config page
-    self.notesToShow = self.urlNotes;
+    self.deleteNote = function(note) {
+      note.$remove();
+
+      for (var i in self.domainNotes) {
+        if (self.domainNotes[i] === note) {
+          self.domainNotes.splice(i,1);
+          self.generateUrlNotes(self.domainNotes, self.defineNotesToShow());
+        }
+      }
+    };
 
   }
 
