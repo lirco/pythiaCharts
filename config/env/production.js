ba6fbdd5d287444acefa72298e653c7029e78168
lirco <lirco77@gmail.com>
2015-06-05 19:47:16 +0300

    added angular material for website

--- config/env/production.js
+++ config/env/production.js
@@ -6,7 +6,8 @@ module.exports = {
 		lib: {
 			css: [
 				'public/lib/bootstrap/dist/css/bootstrap.min.css',
-				'public/lib/bootstrap/dist/css/bootstrap-theme.min.css'
+				'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+				'public/lib/angular-material/angular-material.css'
 			],
 			js: [
 				'public/lib/angular/angular.min.js',
@@ -17,6 +18,8 @@ module.exports = {
 				'public/lib/angular-sanitize/angular-sanitize.js', 
 				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
 				'public/lib/angular-ui-utils/ui-utils.min.js',
+				'public/lib/angular-aria/angular-aria.js',
+				'public/lib//angular-material/angular-material.js',
 				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js'
 			]
 		},
